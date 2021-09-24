ad628fb06367eb993e9743e3b4631fdc125a8924
A H Zahin <azahinhasan@gmail.com>
2021-09-24 11:52:44 +0600

    done!!

--- src/components/afterLogin/homePage.js
+++ src/components/afterLogin/homePage.js
@@ -94,11 +94,33 @@ const HomePage=()=> {
     useEffect(()=>{
         //localStorage.setItem('userID','01');
         setMsg('');
-
+        setResult('');
+        console.log('useEffect');
     },[])
 
 
 
+    let Instructions= ( 
+            <div className="instructions">
+                <h3>Instructions </h3>
+                <div>
+                    1. In Input Values user can input like 1, 2 or 1,2 or 1.2 or 1 2 etc.
+                    <br/>
+                    2.If you give Alphabet in input it will not store in DB.
+                    Such as: 
+                        <br/>
+                        &nbsp;&nbsp; Input Given: 1, 2, A , 3  
+                        <br/>
+                        &nbsp;&nbsp; Will store in DB: 3,2,1
+                    <br/>
+                    3. In Privious Values page it will show by defult all privious input values of that user.
+                    <br/>
+                    4.User can filter by Start Datetime and End Datetime
+                    <br/>
+                    5.If user is not enterd any input values and Is not press on Khoj button there will be Emapty msg in Previous page
+                </div>
+            </div>
+    )
 
 
     let pageData = '';
@@ -112,12 +134,12 @@ const HomePage=()=> {
                     <table style={{fontWeight:'bold',width:'80%'}}>
                         <tr>
                             <td>Input values: </td>
-                            <td><input onChange={e=>setInputValue(e.target.value)}/> </td>
+                            <td><input onChange={e=>setInputValue(e.target.value)}  value={inputValue}/> </td>
                         
                         </tr>
                         <tr>
                             <td>Search values: </td>
-                            <td><input onChange={e=>setSearchValue(e.target.value)} type='number'/> </td>
+                            <td><input onChange={e=>setSearchValue(e.target.value)} type='number' value={searchValue}/> </td>
                         </tr>
                         <tr>
                             <td style={{color:'#9A00FF'}}>{msg} </td>
@@ -128,6 +150,9 @@ const HomePage=()=> {
                             <td style={{color:result=='False'?'red':'green',fontWeight:'bold'}}>{result}</td>
                         </tr> 
                     </table>
+
+                    {Instructions}
+
                 </div>
     }
     else{
@@ -136,22 +161,25 @@ const HomePage=()=> {
         
     }
 
-  return (
-    <div className="App">
-        
-        <h2>Welcome {localStorage.getItem('email')}</h2>
+    return (
+        <div className="App">
+            
+            <h2>Welcome {localStorage.getItem('email')}</h2>
+            <br/>
 
-        <button onClick={()=>setShowSearchPage(true)}>Search Page</button>
-        <button onClick={()=>setShowSearchPage(false)}>Previous Values</button>
-
-        <button onClick={()=>logOut()} style={{backgroundColor:'red',borderColor:'red'}}>LogOut</button>
-
-
+            <button onClick={()=>setShowSearchPage(true)}>Search Page</button>
+            <button onClick={()=>setShowSearchPage(false)}>Previous Values</button>
+            <button onClick={()=>logOut()} style={{backgroundColor:'red',borderColor:'red'}}>LogOut</button>
+            
+            <br/>
 
             {pageData}
 
-    </div>
-  );
+           
+
+
+        </div>
+    );
 }
 
 export default HomePage;
